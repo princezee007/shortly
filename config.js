8e2b4bf55c83b06ae78a2ce16cbe879ce4c00770
Zeshan Malik <124659795+princezee007@users.noreply.github.com>
2025-08-18 12:56:59 +0500

    Remove duplicate config.js from root directory

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  BASE_URL: process.env.NODE_ENV === 'production'
-    ? process.env.BASE_URL || 'https://yourdomain.com'
-    : process.env.BASE_URL || 'http://localhost:3001'
-};
